6d53e8477507f6bd181d61c8e150ec3b81a4cb7c
shukla07052004-eng <shukla07052004@gmail.com>
2026-08-05 21:47:35 +0530

    creatiing next.js app and making user model

---
+++ src/models/userModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model.users || mongoose.model("users", userSchema);
