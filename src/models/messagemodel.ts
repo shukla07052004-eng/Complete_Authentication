ce0d6717c028668b85043b7c9c513001c22971b8
shukla07052004-eng <shukla07052004@gmail.com>
2026-08-09 12:23:07 +0530

    First model on typescript

---
+++ src/models/messagemodel.ts
@@ -0,0 +1,67 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+export const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    Token: string;
+    TokenExpiry: Date;
+    forgotPasswordToken: string;
+    forgotPasswordTokenExpiry: Date;
+    verifyToken: string;
+    verifyTokenExpiry: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const UserSchema: Schema<User> = new Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true
+        },
+        Token: String,
+        TokenExpiry: Date,
+        forgotPasswordToken: String,
+        forgotPasswordTokenExpiry: Date,
+        verifyToken: String,
+        verifyTokenExpiry: Date
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default { MessageSchema };
+
+export const User =
+    mongoose.models.User || mongoose.model<User>("User", UserSchema);
