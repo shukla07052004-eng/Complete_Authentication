199809d8ff1bb2214883fa1c188ac2d4ca981670
shukla07052004-eng <shukla07052004@gmail.com>
2026-08-10 19:43:26 +0530

    Making next.js compatible database connection

---
+++ src/lib/dbconnect.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectObject = {}
+
+async function connectDB(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already connected to the database")
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URL! || '', {})
+        
+        connection.isConnected = db.connections[0].readyState;
+
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log("Failed to connect DB", error)
+    }
+}
+
+export default connectDB
